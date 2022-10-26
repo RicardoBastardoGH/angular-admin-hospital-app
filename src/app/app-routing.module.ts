6721700328119ed5d7456bfd2dadfe9d322e11a1
Ricardo Bastardo <bastvai@gmail.com>
2022-10-26 13:09:08 -0400

    Estructura principal sin modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NonPageFoundComponent } from './pages/non-page-found/non-page-found.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Chart1Component } from './pages/chart1/chart1.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'chart1', component: Chart1Component },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ]
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: '**', component: NonPageFoundComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/non-page-found/non-page-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-non-page-found',
+  templateUrl: './non-page-found.component.html',
+  styleUrls: ['./non-page-found.component.scss']
+})
+export class NonPageFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
